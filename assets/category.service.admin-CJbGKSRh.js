d7e5e7a38eb2ac067a8f26aa19ff516156ce3e38
xgshing <xgshing@qq.com>
2026-01-02 10:17:32 +0800

    Updates

--- assets/category.service.admin-CJbGKSRh.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e}from"./index-DzFIMJFo.js";function a(){return e.get("/admin/categories/tree/").then(t=>t.data)}function o(t){return e.post("/admin/categories/",t)}export{o as c,a as f};
---
+++ assets/company-C1sozY0a.js
@@ -0,0 +1,1 @@
+import{Y as o}from"./index-QDArsuHU.js";const t=()=>o.get("/company/profile/"),p=e=>o.post("/company/profile/",e);export{t as g,p as s};
