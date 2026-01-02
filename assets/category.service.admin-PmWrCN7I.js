713fc36cc2f31a5804cbd50b4d8da0dccc87003a
xgshing <xgshing@qq.com>
2026-01-02 18:30:12 +0800

    Updates

---
+++ assets/category.service.admin-PmWrCN7I.js
@@ -0,0 +1,1 @@
+import{Y as t}from"./index-4MrWQ5yv.js";function a(){return t.get("/admin/categories/tree/").then(e=>e.data)}function o(e){return t.post("/admin/categories/",e)}export{o as c,a as f};
